b2303fa4c19190adc53ae3faa03972020386db10
Natalia <53310862+ripleycmd@users.noreply.github.com>
2021-06-07 21:06:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://docs.amplify.aws/"
           target="_blank"
           rel="noopener noreferrer"
         >
